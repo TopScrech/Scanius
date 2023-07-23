1f78d63382b91c31a7a237aa8b83fa8078d81af1
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-07-23 20:39:59 +0200

    Initial Commit

---
+++ Scanius/ScaniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaniusApp.swift
+//  Scanius
+//
+//  Created by Sergei Saliukov on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScaniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
